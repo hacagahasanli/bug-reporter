a1cbc896e031c202c174c38f2c4de810f13bc8bd
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-03-11 23:33:49 +0400

    Bug Reporter

---
+++ assets/js/BugReporter.js
@@ -0,0 +1,35 @@
+// document
+//     .querySelector("#ozz")
+//     .addEventListener("click", function () {
+//         let element = document.getElementById("new");
+//         var yeniDev = document.createElement("div");
+//         yeniDev.setAttribute("class", "card cardDiv")
+//         element.appendChild(yeniDev);
+//         let insideImg = document.createElement("img");
+//         insideImg.setAttribute("class", "card-img-top image");
+//         insideImg.setAttribute("src", "./Black.jpeg");
+//         insideImg.setAttribute("alt", "Card image cap");
+//         yeniDev.appendChild(insideImg);
+//         let insideDiv = document.createElement("div");
+//         insideDiv.setAttribute("class", "card-body bodyCard");
+//         yeniDev.appendChild(insideDiv);
+//         let h5 = document.createElement("h5");
+//         let text = document.createTextNode("Isimiz hele bitmedi")
+//         h5.appendChild(text);
+//         h5.setAttribute("class", "card-title");
+//         insideDiv.appendChild(h5);
+//         let p = document.createElement("p");
+//         let textP = document.createTextNode(
+//             " Aiq ve men olan yerde biz bitdi demeden bitmez"
+//         )
+//         p.appendChild(textP);
+//         p.setAttribute("class", "card-text");
+//         insideDiv.appendChild(p);
+//         let a = document.createElement("a");
+//         let textP1 = document.createTextNode("Go Somewhere")
+//         a.appendChild(textP1);
+//         a.setAttribute("class", "btn btn-primary btn-dark");
+//         insideDiv.appendChild(a);
+
+//     });
+
